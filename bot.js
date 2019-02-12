b6467dff1a30c332002e24c42d36d742f419987a
Marius S Kristiansen <mariussk@getacademy.no>
2019-02-12 14:51:49 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -125,11 +125,11 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                             errorOut(channelID, err);
                             return;
                         }
-                        if(args[0] == ' ') {
-                            msg(channelID, '__**It\'s dangerous to go alone, take this:**__\nhttps://discord.gg/'+res.code);
-                        } else if (args[0]) {
+                        if(args[0]) {
                             msg(args[0], 'https://discord.gg/'+res.code, false);
                             msg(channelID, 'Invite sent to userID: ' + args[0]);
+                        } else {
+                            msg(channelID, '__**It\'s dangerous to go alone, take this:**__\nhttps://discord.gg/'+res.code);
                         }
                     })
                     
